537d7c4bfc0ec80db2a574ec823c202a6f3ad7c5
Mubashshir Ansari <ansarimubashir000@gmail.com>
2024-01-31 15:50:35 +0530

    changed upload_stream to upload

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -4,52 +4,47 @@ import { v2 as cloudinary } from "cloudinary";
 export const uploadFiles = async (photo) => {
   cloudinary.config({
     cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+    api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
   });
 
   const arrayBuffer = await photo.arrayBuffer();
-  const buffer = new Uint8Array(arrayBuffer);
+  var mime = photo.type;
+  var encoding = "base64";
+  var base64Data = Buffer.from(arrayBuffer).toString(encoding);
+  var fileUri = "data:" + mime + ";" + encoding + "," + base64Data;
   return new Promise((resolve, reject) => {
     cloudinary.uploader
-      .upload_stream(
-        {
-          folder: "automobile",
-        },
-        function (error, result) {
-          if (error) {
-            reject(error);
-            console.error(error)
-            throw new Error(error)
-            return;
-          }
-          resolve({
-            public_id: result.public_id,
-            url: result.secure_url,
-          });
-        }
-      )
-      .end(buffer);
+      .upload(fileUri, {
+        folder: "automobile",
+      })
+      .then((result) => {
+        console.log(result);
+        resolve({
+          public_id: result.public_id,
+          url: result.secure_url,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        reject(error);
+      });
   });
 };
 
 export const deleteFile = async (id) => {
   cloudinary.config({
     cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+    api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
   });
   return new Promise((resolve, reject) => {
-    cloudinary.uploader
-   .destroy(
-        id,
-        function (error, result) {
-          if (error) {
-            reject(error);
-            return;
-          }
-          resolve(result);
-        }
-      );
+    cloudinary.uploader.destroy(id, function (error, result) {
+      if (error) {
+        reject(error);
+        return;
+      }
+      resolve(result);
+    });
   });
-}
+};
